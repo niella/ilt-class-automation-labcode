c4d9e30a0113c4a3afeaccfcba6f4695ce47d8f4
NeilJvR <neil.vanrensburg@gmail.com>
2023-05-05 09:01:51 +0200

    Add files via upload

--- dozer/dozerSetup.sh
+++ dozer/dozerSetup.sh
@@ -1,1 +1,24 @@
-test
+# Add Morpheus DB IP to mysql.env dile
+
+APPLIANCEIP=<%=customOptions.appIP%>
+sed -i 's/MORPHDBIP/'"$APPLIANCEIP"'/' /opt/dozer/mysql.env
+
+# Set execute permission on dozer
+chmod 764 /opt/dozer/dozer
+
+# Create systemd service file
+cat <<EOF >> /etc/systemd/system/dozer.service
+[Unit]
+Description=dozer service
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=root
+ExecStart=/opt/dozer/dozer
+
+[Install]
+WantedBy=multi-user.target
+EOF
